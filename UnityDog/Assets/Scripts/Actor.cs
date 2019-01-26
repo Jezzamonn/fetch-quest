7bdcf17b3708da309607cdfe4f59906707560936
Caleb Markley <cablay91@gmail.com>
2019-01-25 22:24:13 -0800

    Initial Interactions

---
+++ UnityDog/Assets/Scripts/Actor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    private Dictionary<Collider, Interactable> nearbyObjects = new Dictionary<Collider, Interactable>();
+    private Interactable lastClosest;
+
+    private void Update()
+    {
+        UpdateClosest();
+    }
+
+    private void UpdateClosest()
+    {
+        Interactable closestObject = null;
+        float minDistance = float.MaxValue;
+        foreach (KeyValuePair<Collider, Interactable> thing in nearbyObjects)
+        {
+            float distance = (transform.position - thing.Key.ClosestPointOnBounds(transform.position)).sqrMagnitude;
+            if (distance < minDistance)
+            {
+                closestObject = thing.Value;
+                minDistance = distance;
+            }
+        }
+
+        if (closestObject == lastClosest)
+        {
+            return;
+        }
+
+        lastClosest?.NotContextObject();
+        closestObject?.IsContextObject();
+
+        lastClosest = closestObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactable interactable = other.GetComponent<Interactable>();
+        if (interactable != null && !nearbyObjects.ContainsKey(other))
+        {
+            nearbyObjects.Add(other, interactable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (nearbyObjects.ContainsKey(other))
+        {
+            nearbyObjects.Remove(other);
+        }
+    }
+}
---
+++ UnityDog/Assets/Scripts/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private Material normalMat;
+    [SerializeField] private Material closestMat;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void IsContextObject()
+    {
+        meshRenderer.material = closestMat;
+    }
+
+    public void NotContextObject()
+    {
+        meshRenderer.material = normalMat;
+    }
+}
