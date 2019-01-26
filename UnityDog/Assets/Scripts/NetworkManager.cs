fb668104bb14e6fab2b8a3fe54e8a65da940f731
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-01-26 15:06:16 -0800

    Trigger the reactions based on server responses

--- UnityDog/Assets/Scripts/NetworkManager.cs
+++ UnityDog/Assets/Scripts/NetworkManager.cs
@@ -6,23 +6,43 @@ using SocketIO;
 public class NetworkManager : MonoBehaviour
 {
 
+    private static NetworkManager instance;
+
     public SocketIOComponent socket;
+
+    void Awake()
+    {
+        // Check if instance already exists
+        if (instance == null)
+            // if not, set instance to this
+            instance = this;
+        
+        // If instance already exists and it's not this:
+        else if (instance != this)
+            // Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        // Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         socket.On("reaction", HandleReaction);
-        // var socket = IO.Socket("http://localhost:3000");
-        // socket.On(Socket.EVENT_CONNECT, () => {
-        //     // TODO: Connect saying this is a player
-        //     socket.Emit("hi");
-        // });
-        // socket.On("reaction", (message) => {
-        //     Debug.Log(message);
-        // });
     }
 
     void HandleReaction(SocketIOEvent socketIOEvent) {
-        Debug.Log("we got something...");
-        Debug.Log(socketIOEvent.data.ToString());
+        string reaction = socketIOEvent.data.ToString();
+
+        if (reaction.Contains("good")) {
+            FeedbackManager.feedbackQueue.Enqueue(true);
+        }
+        else if (reaction.Contains("bad")) {
+            FeedbackManager.feedbackQueue.Enqueue(false);
+        }
+        else {
+            Debug.LogError("Don't know how to deal with reaction " + reaction);
+        }
     }
 }
