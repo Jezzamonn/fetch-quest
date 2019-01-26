7a481edeb2191daaf295ab08971344d8c2c8cfc5
Caleb Markley <cablay91@gmail.com>
2019-01-26 09:07:39 -0800

    Add self-registering zones

---
+++ UnityDog/Assets/Scripts/Zone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Zone : MonoBehaviour
+{
+    [SerializeField] private ZoneManager.ZoneId zoneId;
+
+    private void Awake()
+    {
+        Collider collider = GetComponent<Collider>();
+
+        if (ZoneManager.zones.ContainsKey(zoneId))
+        {
+            Debug.LogErrorFormat("Tried to add zoneId {0} for {1} that already exists for {2}.",
+                zoneId,
+                collider.gameObject.name,
+                ZoneManager.zones[zoneId].gameObject.name);
+        }
+
+        ZoneManager.zones.Add(zoneId, collider);
+    }
+
+    private void OnDestroy()
+    {
+        ZoneManager.zones.Remove(zoneId);
+    }
+}
