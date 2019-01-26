f914cf373e9ce92f0653db9fecd01ed4e42a1b93
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-01-26 11:22:37 -0800

    OK! Got mobile communication working!

---
+++ UnityDog/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    public SocketIOComponent socket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket.On("reaction", HandleReaction);
+        // var socket = IO.Socket("http://localhost:3000");
+        // socket.On(Socket.EVENT_CONNECT, () => {
+        //     // TODO: Connect saying this is a player
+        //     socket.Emit("hi");
+        // });
+        // socket.On("reaction", (message) => {
+        //     Debug.Log(message);
+        // });
+    }
+
+    void HandleReaction(SocketIOEvent socketIOEvent) {
+        Debug.Log("we got something...");
+        Debug.Log(socketIOEvent.data.ToString());
+    }
+}
