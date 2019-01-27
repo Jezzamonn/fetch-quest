5d49a857aab426ada09b400dfeb3c959317ca920
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-01-27 15:00:24 -0800

    Add a hand to pat the good dog

---
+++ UnityDog/Assets/Scripts/PatHand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatHand : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve animCurve;
+
+    private RectTransform rectTransform;
+    private readonly Timer timer = new Timer();
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        EventManager.onGoalDone.Register(OnGoalDone);
+    }
+
+    private void Destroy()
+    {
+        EventManager.onGoalDone.Unregister(OnGoalDone);
+    }
+
+    private void OnGoalDone()
+    {
+        timer.Start(FeedbackUI.GetLastCurveTime(animCurve));
+    }
+
+    private void Update()
+    {
+        timer.Tick(Time.deltaTime);
+        Vector2 position = rectTransform.anchoredPosition;
+        position.x = animCurve.Evaluate(timer.ClampedTime());
+        rectTransform.anchoredPosition = position;
+    }
+
+}
