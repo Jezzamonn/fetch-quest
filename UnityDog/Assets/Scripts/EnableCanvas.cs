10c08b0de9a674c7c98c5cfca588f9f5e53761fc
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-01-27 12:53:56 -0800

    Make things a little bigger

---
+++ UnityDog/Assets/Scripts/EnableCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvas : MonoBehaviour
+{
+    public Canvas canvas;
+
+    void Awake()
+    {
+        canvas.enabled = true;
+    }
+}
