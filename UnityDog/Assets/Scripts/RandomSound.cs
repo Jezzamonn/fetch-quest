a77486889d9aafc6dbcb409a0ee3563966ec6407
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-01-27 12:03:48 -0800

    Add a bunch of yes/no sound effects

---
+++ UnityDog/Assets/Scripts/RandomSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSound : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip[] AudioClips;
+
+    private void OnEnable()
+    {
+        AudioClip clip = AudioClips[Random.Range(0, AudioClips.Length)];
+        AudioSource.clip = clip;
+        AudioSource.Play();
+    }
+}
