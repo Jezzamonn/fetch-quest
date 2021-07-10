e89eb1a72201e3032a56eda3d67af41b89dcad36
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-07-09 23:11:12 -0700

    Hide the top level when Tuna goes down.

---
+++ UnityDog/Assets/Scripts/LevelHider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHider : MonoBehaviour
+{
+
+    public GameObject top;
+    public Transform tuna;
+
+    public float cutOffPosition = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tuna.position.y > cutOffPosition) {
+            top.active = true;
+        }
+        else {
+            top.active = false;
+        }
+    }
+}
