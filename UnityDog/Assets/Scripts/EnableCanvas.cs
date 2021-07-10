7edc20662cb99d2b821292c771ef62c0a841a643
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-07-09 22:45:56 -0700

    Create more of the house. Also create Tuna dog controller

--- UnityDog/Assets/Scripts/EnableCanvas.cs
+++ UnityDog/Assets/Scripts/EnableCanvas.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class EnableCanvas : MonoBehaviour
 {
-    public Canvas canvas;
+    public GameObject canvas;
 
     void Awake()
     {
-        canvas.enabled = true;
+        canvas.active = true;
     }
 }
